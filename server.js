2468b8440d2e41873d52f32f1c9e3039e2c0d68a
paulolimauai <paulolp0101@gmail.com>
2026-08-15 17:09:54 -0300

    Delete server.js

--- server.js
+++
@@ -1,8 +0,0 @@
-const app = require('./index');
-
-require('dotenv').config()
-
-const port = 3000 || process.env.SERVERPORT;
-app.listen(port, () => {
-    console.log(`http://localhost:${port}`);
-});
